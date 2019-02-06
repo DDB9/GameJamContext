6e8bc0c60484899d3d531cd3648c8d16187967a7
Daan de Bruijn <ddebruijn9@gmail.com>
2019-02-06 12:08:09 +0100

    Tussentijdste push
    Bukra do your thang

---
+++ ContextJam/Assets/Scripts/Bucket.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bucket : MonoBehaviour, IInventoryItem
+{
+    public string name
+    {
+        get
+        {
+            return "Bucket";
+        }
+    }
+
+    public Sprite _Image = null;
+    public Sprite image
+    {
+        get
+        {
+            return _Image;
+        }
+    }
+    public void OnPickup()
+    {
+        gameObject.SetActive(false);
+    }
+}
