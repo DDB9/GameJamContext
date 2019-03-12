020bce8269b6e12a102f16c239237a9d1d35a235
Daan de Bruijn <ddebruijn9@gmail.com>
2019-03-12 13:35:41 +0100

    Working on the farm grid

---
+++ ContextJam/Assets/Scripts/SpacialDestruction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpacialDestruction : MonoBehaviour
+{
+    public Inventory inventory;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Occupied"))
+        {
+            Transform parent = this.transform.parent;
+            IInventoryItem item = parent.GetComponent<IInventoryItem>();
+
+            if (item != null)
+            {
+                inventory.AddItem(item);
+            }
+            Destroy(parent);
+            
+        }
+    }
+}
