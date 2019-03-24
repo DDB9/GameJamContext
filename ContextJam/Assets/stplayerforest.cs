9a0b93e11c923b71b8d9d15e78db6bfd892c661c
Stijn de Koning <sdk.stijn@gmail.com>
2019-03-24 20:52:35 +0100

    nnewwwwmusccc

---
+++ ContextJam/Assets/stplayerforest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stplayerforest : MonoBehaviour
+{
+
+    FMOD.Studio.EventInstance OST;
+    FMOD.Studio.ParameterInstance fullspanning;
+    FMOD.Studio.ParameterInstance fullvolume;
+    FMOD.Studio.ParameterInstance currentscene;
+
+    public StaminaHealthBarManager shbm;
+
+    void Awake()
+    {
+        OST = FMODUnity.RuntimeManager.CreateInstance("event:/Music All");
+        OST.getParameter("fullspanning", out fullspanning);
+        OST.getParameter("fullvolume", out fullvolume);
+        OST.getParameter("currentscene", out currentscene);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(OST, GetComponent<Transform>(), GetComponent<Rigidbody>());
+        OST.start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fullvolume.setValue(0.7f);
+        currentscene.setValue(3);
+
+        if (shbm.hunger < 30)
+        {
+            fullspanning.setValue(1);
+        }
+        else
+        {
+            fullspanning.setValue(0);
+        }
+    }
+}
