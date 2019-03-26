2593a41e96fd6848d3bfee48d0bf447621885102
Daan de Bruijn <ddebruijn9@gmail.com>
2019-03-26 12:28:39 +0100

    Revert "a"
    This reverts commit 1f225acdcf4563fe360fb008ee147f520b190906.

--- ContextJam/Assets/STPlayer.cs
+++ ContextJam/Assets/STPlayer.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class STPlayer : MonoBehaviour
 {
@@ -12,12 +11,6 @@ public class STPlayer : MonoBehaviour
     FMOD.Studio.ParameterInstance currentscene;
 
     public StaminaHealthBarManager shbm;
-
-    public loadwhichscene lws;
-    Scene currentScene = SceneManager.GetActiveScene();
-    //string sceneName  = currentScene.name;
-    string sceneName;
-
 
     void Awake()
     {
@@ -40,34 +33,16 @@ public class STPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        sceneName = currentScene.name;
-        Debug.Log(SceneManager.GetActiveScene().name);
-
         fullvolume.setValue(0.7f);
+        currentscene.setValue(1);
 
         if (shbm.hunger < 30)
         {
             fullspanning.setValue(1);
-
         }
         else 
         {
             fullspanning.setValue(0);
-        }
-
-
-        //currentscene.setValue(lws.sceneNumber);
-        if (sceneName == "Farm")
-        {
-            currentscene.setValue(1);
-        }
-        else if (sceneName == "Lake")
-        {
-            currentscene.setValue(2);
-
-        }
-        else {
-            currentscene.setValue(3);
         }
     }
 }
